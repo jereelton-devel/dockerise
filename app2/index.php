25a01add5ca947d71519b6172e4a02412d0b161e
Jereelton Teixeira <jereeltondevel@gmail.com>
2021-07-25 21:59:42 -0300

    Initial Commit

---
+++ app2/index.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+echo "SESSION START<br/>";
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>App2 Dockerise</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/dark.min.css">
+</head>
+<body>
+
+    <h1>App2:This is a Docker Compose Demo Page.</h1>
+    <p>This content is being served by an Nginx container.</p>
+
+</body>
+</html>
